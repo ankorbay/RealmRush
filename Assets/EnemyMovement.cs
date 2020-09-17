42af8552b172cba24a58fa46370d053e35d02c6d
Anton Korshunov <anton.korshunov@banuba.com>
2020-09-17 11:49:38 +0300

    enemy walking to the end with logging

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{   
+    [SerializeField] List<WayPoint> path;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Starting patrol");
+        StartCoroutine(PrintAllWaypoints());
+    }
+
+    IEnumerator PrintAllWaypoints(){
+        foreach (WayPoint waypoint in path)
+        {
+            transform.position = waypoint.transform.position;
+            print("Visiting position " + waypoint);
+            yield return new WaitForSeconds(1f);
+        }
+        print("Patrol is over");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
